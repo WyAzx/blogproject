532c54008e1e385aab4fa2ecc147acca903069f3
WangYun <wy15810523230@126.com>
2018-05-08 12:27:29 +0800

    fix setting

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -64,9 +64,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'libraries': {
-                'blog_tags': 'blog.templatetags.blog_tags',
-            }
         },
     },
 ]
