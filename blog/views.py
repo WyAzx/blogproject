3381858aeab0f4c48a51eae3633d0e8e3021b3c0
WangYun <wy15810523230@126.com>
2018-05-08 15:45:39 +0800

    add many again

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post
+from comments.forms import CommentForm
+from .models import Post, Category
 
 
 def index(request):
-    post_list = Post.objects.all().order_by('-create_time')
+    post_list = Post.objects.all()
     return render(request,  'blog/index.html', context={'post_list': post_list})
 
 
@@ -17,4 +18,23 @@ def detail(request, pk):
                                      'markdown.extensions.codehilite',
                                      'markdown.extensions.toc',
                                   ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(create_time__year=year,
+                                    create_time__month=month
+                                    )
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
