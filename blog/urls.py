3ce44b7546359fb6cfd9d97e6b452ede1ccae641
WangYun <wy15810523230@126.com>
2018-05-19 12:41:33 +0800

    add PostDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from blog.views import IndexView, ArchivesView, CategoryView
+from blog.views import IndexView, ArchivesView, CategoryView, PostDetailView
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('post/<int:pk>/', views.detail, name='detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='detail'),
     path('archives/<int:year>/<int:month>', ArchivesView.as_view(), name='archives'),
     path('category/<int:pk>', CategoryView.as_view(), name='categories'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from comments.forms import CommentForm
 from .models import Post, Category
@@ -12,22 +12,35 @@ class IndexView(ListView):
     context_object_name = 'post_list'
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.increase_views()
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                     'markdown.extensions.extra',
-                                     'markdown.extensions.codehilite',
-                                     'markdown.extensions.toc',
-                                  ])
-    form = CommentForm()
-    comment_list = post.comment_set.all()
-    context = {'post': post,
-               'form': form,
-               'comment_list': comment_list
-               }
-    return render(request, 'blog/detail.html', context=context)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, args, kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super().get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+                                          'markdown.extensions.toc',
+                                      ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list,
+        })
+        return context
 
 
 class ArchivesView(IndexView):
