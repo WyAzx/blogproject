c3d80344b2ce8e54798de211fb2a0349dc307c96
WangYun <wy15810523230@126.com>
2018-06-01 13:36:01 +0800

    deploy

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'omiyj18f@oeq_7hy0am3w+ei!jp*&%b@q^ghv%j@6d1x55htq)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ec2-13-115-51-188.ap-northeast-1.compute.amazonaws.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '13.231.37.211']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 HAYSTACK_CONNECTIONS = {
     'default': {
--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 python /app/manage.py collectstatic --noinput
+python /app/manage.py migrate
+
 /usr/local/bin/gunicorn blogproject.wsgi -w 4 -b 127.0.0.1:8000 --chdir=/app
