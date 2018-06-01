0a6f1e252b4682b52b060be4c5dc44d9cb95e2bd
WangYun <wy15810523230@126.com>
2018-06-01 17:27:14 +0800

    deploy

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -2,4 +2,4 @@
 python /app/manage.py collectstatic --noinput
 python /app/manage.py migrate
 
-/usr/local/bin/gunicorn blogproject.wsgi:application -w 4 -b 127.0.0.1:8000 --chdir=/app
+/usr/local/bin/gunicorn blogproject.wsgi:application -w 4 -b 127.0.0.1:8011 --chdir=/app
